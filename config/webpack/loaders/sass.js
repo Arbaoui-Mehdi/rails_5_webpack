3e5b0524ff0bacc455de72769e7e9bdf09e694cb
Mehdi <whithat87@gmail.com>
2017-06-13 12:35:36 +0000

    Postcss-loader Install & Config

--- config/webpack/loaders/sass.js
+++ config/webpack/loaders/sass.js
@@ -6,10 +6,25 @@ module.exports = {
   use: ExtractTextPlugin.extract({
     fallback: 'style-loader',
     use: [
-      { loader: 'css-loader', options: { minimize: env.NODE_ENV === 'production' } },
-      { loader: 'postcss-loader', options: { sourceMap: true } },
+      {
+        loader: 'css-loader', options: { minimize: env.NODE_ENV === 'production' }
+      },
       'resolve-url-loader',
-      { loader: 'sass-loader', options: { sourceMap: true } }
+      {
+        loader: 'postcss-loader?parser=sugarss', options: {
+          plugins: [
+            require("precss"),
+            require("autoprefixer"),
+            require("postcss-apply"),
+            require("postcss-calc"),
+            require("postcss-font-magician")
+          ],
+          sourceMap: true
+        }
+      },
+      {
+        loader: 'sass-loader', options: { sourceMap: true }
+      }
     ]
   })
 }
